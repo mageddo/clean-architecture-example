2bfe235d9f30ac8c7aef811fd9bf10dcc726947a
Elvis de Freitas <edigitalb@gmail.com>
2019-10-31 00:28:53 -0300

    upgrading gradle and spring

--- application/configuration/src/main/java/com/clean/example/Application.java
+++ application/configuration/src/main/java/com/clean/example/Application.java
@@ -2,12 +2,11 @@ package com.clean.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "com.clean.example.configuration")
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
--- application/configuration/src/main/java/com/clean/example/configuration/EndpointConfiguration.java
+++ application/configuration/src/main/java/com/clean/example/configuration/EndpointConfiguration.java
@@ -7,7 +7,7 @@ import com.clean.example.entrypoints.rest.exchange.capacity.GetCapacityForExchan
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class EndpointConfiguration {
 
     @Bean
